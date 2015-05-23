f0b4d2aa49f1361ee5b4f6bad0af7aa22cd70ed4
Andrea Stagi <stagi.andrea@gmail.com>
2015-05-23 11:07:19 +0200

    Add parallel capture the flag example

---
+++ capturetheflag/parallel_ctf.py
@@ -0,0 +1,54 @@
+from random import randint
+from threading import Thread
+import time
+
+class Flag():
+
+    def __init__(self):
+        self._captured = False
+        self._available = False
+
+    @property
+    def captured(self):
+        return self._captured
+
+    @property
+    def available(self):
+        return self._available
+
+    def set_captured(self):
+        self._captured = True
+
+    def set_available(self):
+        self._available = True
+
+
+class Player(Thread):
+
+    def __init__(self, name, flag):
+        self._name = name
+        self._flag = flag
+        Thread.__init__(self)
+
+    def run(self):
+        while not self._flag.available:
+            pass
+        if not self._flag.captured:
+            time.sleep(randint(0,3))
+            self._flag.set_captured()
+            print ('{0} WIN!'.format(self._name))
+        else:
+            print ('{0} IS A LOSER!'.format(self._name))
+
+
+flag = Flag()
+
+player_1 = Player('A', flag)
+player_2 = Player('B', flag)
+player_3 = Player('C', flag)
+
+player_1.start()
+player_2.start()
+player_3.start()
+
+flag.set_available()
